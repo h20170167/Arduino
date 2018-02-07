08d2e67a5ba93d56ff6d0239802b19d2942709ce
Sarthi Chugh <35374688+h20170165@users.noreply.github.com>
2018-02-07 19:36:29 +0530

    initial commit

---
+++ sketch_feb06a.ino
@@ -0,0 +1,26 @@
+/*
+SimpleReceive
+This sketch displays text strings received using VirtualWire
+Connect the Receiver data pin to Arduino pin 11
+*/
+#include <VirtualWire.h>
+byte msg[VW_MAX_MESSAGE_LEN]; // a buffer to store the incoming message
+byte len=VW_MAX_MESSAGE_LEN;
+void setup()
+{
+Serial.begin(9600);
+Serial.println("Device is ready");
+// Initialize the IO and ISR
+vw_setup(2000); // Bits per sec
+vw_rx_start(); // Start the receiver
+}
+void loop()
+{
+if (vw_get_message(msg, &len))
+{
+for(int i=0;i<len;++i)
+Serial.print(char(msg[i]));
+Serial.println();
+}
+}
+
---
+++ sketch_feb07a.ino
@@ -0,0 +1,38 @@
+#include <VirtualWire.h>
+char msg[3];
+int duration;
+int distance;
+int count;
+void setup()
+{
+pinMode(8,OUTPUT);
+pinMode(3,INPUT);
+Serial.begin(9600);
+vw_setup(2000); // Bits per sec
+count=0;
+}
+void loop()
+{
+digitalWrite(8,LOW);
+delayMicroseconds(2);
+digitalWrite(8,HIGH);
+delayMicroseconds(10);
+digitalWrite(8,LOW);
+
+duration=pulseIn(3,HIGH);
+distance=(duration*.0343)/2;
+if(count==0)
+txmit_distance(distance);
+count=(count+1)%100;
+}
+void txmit_distance(int distance)
+{
+   Serial.println(distance);
+   msg[2]=distance%10+48;
+   distance/=10;
+   msg[1]=distance%10+48;
+   msg[0]=distance/10+48;
+   vw_send((uint8_t *)msg, strlen(msg));
+   vw_wait_tx();
+}
+
